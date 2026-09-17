a6a0122b998f60f34e7a3298f0d488cab1b4206f
Mathi <mathias.rodriguez.agro@dualbootpartners.com>
2026-09-17 18:02:50 -0300

    Traer navegación autónoma desde repositorio externo

--- setup/build.sh
+++ setup/build.sh
@@ -4,7 +4,7 @@ source /opt/ros/jazzy/setup.bash
 cd /ros2_ws
 if [ "$#" -eq 0 ]; then
   colcon build --symlink-install --packages-up-to \
-    stage_autonomous_nav stage_pid_navigation turtleboot3_autonomous_nav turtle_py
+    stage_autonomous_nav autonomous_navigation_pria turtleboot3_autonomous_nav turtle_py
 else
   colcon build --symlink-install "$@"
 fi
